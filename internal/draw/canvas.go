f97296b954d673378571a2817d09e851cca884e3
Mario Macias <mmacias@newrelic.com>
2020-03-15 00:13:13 +0100

    implemented some functions in canvas

--- internal/draw/canvas.go
+++ internal/draw/canvas.go
@@ -10,6 +10,25 @@ type Canvas struct {
 	ctx    js.Value
 }
 
+type Cap string
+
+const (
+	LineCapButt   Cap = "butt"
+	LineCapRound  Cap = "round"
+	LineCapSquare Cap = "square"
+)
+
+// https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+// TODO: measureText https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/measureText
+// TODO: lineJoin https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineJoin
+// TODO: miterLimit https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/miterLimit
+// TODO: getLineDash, setLineDash, lineDashOffset https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/getLineDash
+// TODO: textAlign, textBaseline, direction
+// TODO: createLinearGradient, createRadialGradient, createPattern
+// TODO: shadowBlur, shadowColor, shadowOffsetX, shadowOffsetY
+// TODO: beginPath, closePath, moveTo, lineTo, bezierCurveTo, quadraticCurveTo, arc, arcTo, ellipse, rect
+// TODO: fill, stroke, drawFocusIfNeeded, scrollPathIntoView, clip, isPointInPath, isPointInStroke
+
 type CanvasConfig func(c *Canvas)
 
 func GetCanvas(id string, cfgs ...CanvasConfig) *Canvas {
@@ -46,7 +65,51 @@ func (c *Canvas) adjustToWindow() {
 	c.elem.Set("height", c.height)
 }
 
-func (c *Canvas) Clear(bgcolor string) {
-	c.ctx.Set("fillStyle", bgcolor)
-	c.ctx.Call("fillRect", 0, 0, c.width, c.height)
+func (c *Canvas) FillRect(x, y, width, height float64) {
+	c.ctx.Call("fillRect", x, y, width, height)
 }
+
+func (c *Canvas) ClearRect(x, y, width, height float64) {
+	c.ctx.Call("clearRect", x, y, c.width, c.height)
+}
+
+func (c *Canvas) StrokeRect(x, y, width, height float64) {
+	c.ctx.Call("strokeRect", x, y, width, height)
+}
+
+func (c *Canvas) FillText(text string, x, y float64) {
+	c.ctx.Call("fillText", text, x, y)
+}
+
+func (c *Canvas) FillTextMaxWidth(text string, x, y, maxWidth float64) {
+	c.ctx.Call("fillText", text, x, y, maxWidth)
+}
+
+func (c *Canvas) StrokeText(text string, x, y float64) {
+	c.ctx.Call("strokeText", text, x, y)
+}
+
+func (c *Canvas) StrokeTextMaxWidth(text string, x, y, maxWidth float64) {
+	c.ctx.Call("strokeText", text, x, y, maxWidth)
+}
+
+func (c *Canvas) LineWidth(width float64) {
+	c.ctx.Set("lineWidth", width)
+}
+
+func (c *Canvas) LineCap(cap Cap) {
+	c.ctx.Set("lineCap", string(cap))
+}
+
+func (c *Canvas) Font(font string) {
+	c.ctx.Set("font", font)
+}
+
+func (c *Canvas) FillStyle(style string) {
+	c.ctx.Set("fillStyle", style)
+}
+
+func (c *Canvas) StrokeStyle(style string) {
+	c.ctx.Set("strokeStyle", style)
+}
+
--- pkg/main.go
+++ pkg/main.go
@@ -10,8 +10,12 @@ func main() {
 	c := draw.GetCanvas("theCanvas",
 		draw.FullScreen(true))
 
+	c.ClearRect(0,0,500,500)
 	for {
-		c.Clear("green")
+		c.StrokeStyle("green")
+		c.LineWidth(15)
+		c.LineCap(draw.LineCapRound)
+		c.StrokeRect(100,100,100,100)
 		time.Sleep(1 * time.Millisecond)
 	}
 
